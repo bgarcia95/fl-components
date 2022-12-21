43f3eb33b3d8c6f727a853aec8c4d9b3ad6904ef
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-21 11:32:13 -0600

    use listview2 screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:fl_components/screens/listview1_screen.dart';
+import 'package:fl_components/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: ListView1Screen(),
+      home: ListView2Screen(),
       debugShowCheckedModeBanner: false,
     );
   }
