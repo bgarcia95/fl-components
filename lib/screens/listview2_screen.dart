1d9cda401f0e4b942ffc06fef4650d94de443ead
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-21 11:32:06 -0600

    add listview2 screen and understand listview.separated (lazy loading list)

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// not a constant widget because it will not be created on build time but runtime.
+
+class ListView2Screen extends StatelessWidget {
+  const ListView2Screen({super.key});
+
+  final games = const ['Megaman', 'Metal Gear', 'Super Smash', 'Final Fantasy'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Type 2 ListView'),
+      ),
+      body: Center(
+        // .separated or .builder creates a lazy loading list
+        child: ListView.separated(
+          itemCount: games.length,
+          itemBuilder: (context, i) => ListTile(
+            title: Text(games[i]),
+            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+        ),
+      ),
+    );
+  }
+}
