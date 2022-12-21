a7a034258f773a0df65c95d25fc818544ec3eebd
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-21 15:23:11 -0600

    add model and routes list

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
 
 import '../screens/screens.dart';
+import 'package:fl_components/models/models.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    // TODO: delete home
+    MenuOption(
+      route: 'home',
+      title: 'Home Screen',
+      screen: const HomeScreen(),
+      icon: Icons.home,
+    ),
+    MenuOption(
+      route: 'listview1',
+      title: 'List View Type 1',
+      screen: const ListView1Screen(),
+      icon: Icons.list_alt,
+    ),
+    MenuOption(
+      route: 'listview2',
+      title: 'List View Type 2',
+      screen: const ListView2Screen(),
+      icon: Icons.list,
+    ),
+    MenuOption(
+      route: 'alert',
+      title: 'Alerts',
+      screen: const AlertScreen(),
+      icon: Icons.add_alert_outlined,
+    ),
+    MenuOption(
+      route: 'card',
+      title: 'Cards',
+      screen: const CardScreen(),
+      icon: Icons.credit_card,
+    ),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (context) => const HomeScreen(),
