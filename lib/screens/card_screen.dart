b27b5ffc7e85fd340490347d1ea2412957bed156
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-08 21:48:05 -0600

    pass parameters to custom card type 2

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,13 +16,21 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10),
-          CustomCardType2(),
-          SizedBox(height: 10),
-          CustomCardType2(),
-          SizedBox(height: 10),
-          CustomCardType2(),
-          SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageUrl:
+                'https://stsci-opo.org/STScI-01G77PGXFM1NRHYEFRD9VMS26R.png',
+          ),
+          SizedBox(height: 20),
+          CustomCardType2(
+            imageUrl:
+                'https://images.theconversation.com/files/125391/original/image-20160606-13080-s7o3qu.jpg?ixlib=rb-1.1.0&rect=0%2C62%2C3200%2C1552&q=45&auto=format&w=1356&h=668&fit=crop',
+          ),
+          SizedBox(height: 20),
+          CustomCardType2(
+            imageUrl:
+                'https://media.macphun.com/img/uploads/customer/blog/504/15360610675b8e6e8b52bd36.49629027.jpg?q=85&w=1680',
+            description: 'A beautiful landscape',
+          ),
           SizedBox(height: 100),
         ],
       ),
