4bd994bd570221cd139ed01a43eadb3ed0e2aff0
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-08 21:47:50 -0600

    receive arguments and conditionally render widgets

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:fl_components/theme/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  final String imageUrl;
+  final String? description;
+
+  const CustomCardType2({super.key, required this.imageUrl, this.description});
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +17,22 @@ class CustomCardType2 extends StatelessWidget {
       shadowColor: AppTheme.primaryColor.withOpacity(0.4),
       child: Column(
         children: [
-          const FadeInImage(
+          FadeInImage(
             image: NetworkImage(
-              'https://stsci-opo.org/STScI-01G77PGXFM1NRHYEFRD9VMS26R.png',
+              imageUrl,
             ),
-            placeholder: AssetImage('assets/jar-loading.gif'),
+            placeholder: const AssetImage('assets/jar-loading.gif'),
             width: double.infinity, // works on widgets where width is limited
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
-          Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            child: const Text('A beaufitul landscape'),
-          )
+          if (description != null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: Text(description!),
+            )
         ],
       ),
     );
