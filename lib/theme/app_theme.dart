caf117ca50de31e211ce041736d07bb7f313cf5b
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-09 09:49:09 -0600

    add new theme styles

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,19 +4,34 @@ class AppTheme {
   static const Color primaryColor = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      // Primary color
-      primaryColor: primaryColor,
-      // Appbar Theme
-      appBarTheme: const AppBarTheme(
-        // backgroundColor: primaryColor,
-        color: primaryColor,
+    // Primary color
+    primaryColor: primaryColor,
+    // Appbar Theme
+    appBarTheme: const AppBarTheme(
+      // backgroundColor: primaryColor,
+      color: primaryColor,
+      elevation: 0,
+    ),
+    // TextButton Theme
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: primaryColor,
+      ),
+    ),
+    // FABs
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primaryColor,
+      elevation: 5,
+    ),
+    // ElevatedButton
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.indigo,
+        shape: const StadiumBorder(),
         elevation: 0,
       ),
-      // TextButton Theme
-      textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-        foregroundColor: primaryColor,
-      )));
+    ),
+  );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
     primaryColor: primaryColor,
