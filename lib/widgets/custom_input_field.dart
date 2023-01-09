fcdf5aa465afdac43273087050e30a7e1fdb1625
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-09 12:22:53 -0600

    add new properties to handle form values

--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -9,6 +9,9 @@ class CustomInputField extends StatelessWidget {
   final TextInputType? keyboardType;
   final bool obscureText;
 
+  final String formProperty;
+  final Map<String, String> formValues;
+
   const CustomInputField({
     Key? key,
     this.hintText,
@@ -18,6 +21,8 @@ class CustomInputField extends StatelessWidget {
     this.suffixIcon,
     this.keyboardType,
     this.obscureText = false,
+    required this.formProperty,
+    required this.formValues,
   }) : super(key: key);
 
   @override
@@ -28,7 +33,7 @@ class CustomInputField extends StatelessWidget {
       keyboardType: keyboardType,
       obscureText: obscureText,
       textCapitalization: TextCapitalization.words,
-      onChanged: (value) => print(value),
+      onChanged: (value) => formValues[formProperty] = value,
       validator: (value) {
         if (value == null) return 'This field is required';
         return value.length < 3 ? 'Min 3 charactees' : null;
