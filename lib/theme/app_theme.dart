5e83748e97cb298af67ff4fdab9f2e395183d7f9
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-08 21:07:30 -0600

    add theme for textbuttons

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,14 +4,19 @@ class AppTheme {
   static const Color primaryColor = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: primaryColor,
-    // Appbar Theme
-    appBarTheme: const AppBarTheme(
-      // backgroundColor: primaryColor,
-      color: primaryColor,
-      elevation: 0,
-    ),
-  );
+      // Primary color
+      primaryColor: primaryColor,
+      // Appbar Theme
+      appBarTheme: const AppBarTheme(
+        // backgroundColor: primaryColor,
+        color: primaryColor,
+        elevation: 0,
+      ),
+      // TextButton Theme
+      textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+        foregroundColor: primaryColor,
+      )));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
     primaryColor: primaryColor,
