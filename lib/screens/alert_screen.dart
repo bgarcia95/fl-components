cf405110e92f7cb7e9bbd0c58063bd6e159e8dfa
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-09 09:48:55 -0600

    add alert widgets based on device OS

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,13 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
+  void displayAlertIOS(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Title'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('This is the alert content'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(size: 100),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void displayAlertAndroid(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Title'),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('This is the alert content'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(size: 100),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+          // style: ElevatedButton.styleFrom(
+          //   elevation: 100,
+          //   backgroundColor: Colors.red,
+          // ),
+          onPressed: () => Platform.isAndroid
+              ? displayAlertAndroid(context)
+              : displayAlertIOS(context),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Show alert', style: TextStyle(fontSize: 16)),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // this will drop the last screen on the screens stack
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.close),
       ),
     );
   }
