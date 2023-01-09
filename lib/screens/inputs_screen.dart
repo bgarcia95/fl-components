5b616ed6e9d6aba0f1aaff6205a56ba7cd10016d
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-09 12:30:06 -0600

    add dropdown field to our form

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -60,6 +60,21 @@ class InputsScreen extends StatelessWidget {
                   formValues: formValues,
                 ),
                 const SizedBox(height: 30),
+                DropdownButtonFormField<String>(
+                  items: const [
+                    DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                    DropdownMenuItem(
+                        value: 'Superuser', child: Text('Superuser')),
+                    DropdownMenuItem(
+                        value: 'Developer', child: Text('Developer')),
+                    DropdownMenuItem(
+                        value: 'Jr. Developer', child: Text('Jr. Developer')),
+                  ],
+                  onChanged: (value) {
+                    print(value);
+                    formValues['role'] = value ?? 'Admin';
+                  },
+                ),
                 ElevatedButton(
                   onPressed: () {
                     FocusScope.of(context).requestFocus(FocusNode());
