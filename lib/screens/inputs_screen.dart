04c945c1bfce80ecfe184436eace97a4b4523a8b
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-09 12:23:39 -0600

    add key to form and access methods. pass new arguments to custom inputs.

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -6,38 +6,82 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // key helps to keep a reference
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Brayan',
+      'last_name': 'Garcia',
+      'email': 'brayan@google.com',
+      'password': '123456',
+      'role': 'Admin',
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs and Forms'),
       ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          children: const [
-            CustomInputField(
-              labelText: 'Name',
-              hintText: 'User name',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Form(
+            key: myFormKey,
+            onChanged: () => {},
+            child: Column(
+              children: [
+                CustomInputField(
+                  labelText: 'Name',
+                  hintText: 'User name',
+                  formProperty: 'first_name',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Lastname',
+                  hintText: 'User last name',
+                  formProperty: 'last_name',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Email',
+                  hintText: 'User email',
+                  keyboardType: TextInputType.emailAddress,
+                  formProperty: 'email',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Password',
+                  hintText: 'User password',
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  formProperty: 'password',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(FocusNode());
+
+                    if (!myFormKey.currentState!.validate()) {
+                      print('Invalid form');
+                      return;
+                    }
+
+                    //* print form values
+                    print(formValues);
+                  },
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(
+                      child: Text('Save'),
+                    ),
+                  ),
+                )
+              ],
             ),
-            SizedBox(height: 30),
-            CustomInputField(
-              labelText: 'Lastname',
-              hintText: 'User last name',
-            ),
-            SizedBox(height: 30),
-            CustomInputField(
-              labelText: 'Email',
-              hintText: 'User email',
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 30),
-            CustomInputField(
-              labelText: 'Password',
-              hintText: 'User password',
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-            ),
-            SizedBox(height: 30),
-          ],
+          ),
         ),
       ), // works as a listview but instead of many widgets we just have one
     );
