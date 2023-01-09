ae24002510b57107ee2c08d52624be04a0efa882
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-08 21:39:02 -0600

    display card type 2 widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -15,6 +15,15 @@ class CardScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         children: const [
           CustomCardType1(),
+          SizedBox(height: 10),
+          CustomCardType2(),
+          SizedBox(height: 10),
+          CustomCardType2(),
+          SizedBox(height: 10),
+          CustomCardType2(),
+          SizedBox(height: 10),
+          CustomCardType2(),
+          SizedBox(height: 100),
         ],
       ),
     );
