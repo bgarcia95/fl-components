f2355ece6a81eb2a7d306b8930598fe402a41115
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-09 12:02:25 -0600

    add new properties

--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -6,6 +6,8 @@ class CustomInputField extends StatelessWidget {
   final String? helperText;
   final IconData? icon;
   final IconData? suffixIcon;
+  final TextInputType? keyboardType;
+  final bool obscureText;
 
   const CustomInputField({
     Key? key,
@@ -14,6 +16,8 @@ class CustomInputField extends StatelessWidget {
     this.helperText,
     this.icon,
     this.suffixIcon,
+    this.keyboardType,
+    this.obscureText = false,
   }) : super(key: key);
 
   @override
@@ -21,6 +25,8 @@ class CustomInputField extends StatelessWidget {
     return TextFormField(
       // autofocus: true,
       initialValue: '',
+      keyboardType: keyboardType,
+      obscureText: obscureText,
       textCapitalization: TextCapitalization.words,
       onChanged: (value) => print(value),
       validator: (value) {
