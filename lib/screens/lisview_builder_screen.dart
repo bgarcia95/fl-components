47d947a438e6bc7d674ddda4eeb748e1c9617bba
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-10 10:50:01 -0600

    convert to stateful widget and add infinite scroll

--- lib/screens/lisview_builder_screen.dart
+++ lib/screens/lisview_builder_screen.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({super.key});
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  // Life cycle -> Whdn the widget is created
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      // print(
+      //     ' ${scrollController.position.pixels},${scrollController.position.maxScrollExtent}');
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        add5();
+      }
+    });
+  }
+
+  void add5() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll(
+      [
+        1,
+        2,
+        3,
+        4,
+        5,
+      ].map((e) => lastId + e),
+    );
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +50,10 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          physics:
+              const BouncingScrollPhysics(), // to make bouncing work on android as well
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, index) {
             return FadeInImage(
               width: double.infinity,
@@ -20,7 +61,7 @@ class ListViewBuilderScreen extends StatelessWidget {
               fit: BoxFit.cover,
               placeholder: const AssetImage('assets/jar-loading.gif'),
               image: NetworkImage(
-                  'https://picsum.photos/500/300?image=${index + 1}'),
+                  'https://picsum.photos/500/300?image=${imagesIds[index]}'),
             );
           },
         ),
